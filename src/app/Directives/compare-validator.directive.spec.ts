763439953850dc84f71bcd1693d2470d3a98655c
Andriy Slesarchuk <slesarchukandr@gmail.com>
2018-11-16 00:43:31 +0200

    Add npx package for future testing. Add a few necessary .spec.ts files

---
+++ src/app/Directives/compare-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CompareValidatorDirective } from './compare-validator.directive';
+
+describe('CompareValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CompareValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
