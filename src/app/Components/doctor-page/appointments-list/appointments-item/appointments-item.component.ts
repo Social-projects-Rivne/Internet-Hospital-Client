f4310540e0735cccb007bb4a64d877ed6fe27b61
Kozachok Sergiy <senjust@gmail.com>
2018-11-22 23:31:45 +0200

    Sebscribed only for patients

--- src/app/Components/doctor-page/appointments-list/appointments-item/appointments-item.component.ts
+++ src/app/Components/doctor-page/appointments-list/appointments-item/appointments-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Appointment } from '../../../DoctorPlans/Appointment';
 import { DoctorplansService } from 'src/app/Components/DoctorPlans/doctorplans.service';
 import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-appointments-item',
@@ -11,10 +13,13 @@ import { ActivatedRoute } from '@angular/router';
 export class AppointmentsItemComponent implements OnInit {
   @Input()
   appointment: Appointment;
+  isPatient: Observable<boolean>;
 
-  constructor(private service: DoctorplansService) { }
+  constructor(private service: DoctorplansService,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.isPatient = this.authenticationService.isPatient();
   }
 
   onSubscribeToAppointment() {
