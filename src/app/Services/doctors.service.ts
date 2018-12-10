a2cf21d7aabb4f0c1689f7793123e6f396a8e6d5
Kozachok Sergiy <senjust@gmail.com>
2018-12-10 10:56:46 +0200

    Removed the commented method

--- src/app/Services/doctors.service.ts
+++ src/app/Services/doctors.service.ts
@@ -106,23 +106,4 @@ export class DoctorsService {
     }
     return this.http.get<MyPatients>(url, this.httpOptions);
   }
-
-  //   getUrl(): string {
-  //     let url = `?${PAGE}=${this.page + 1}`
-  //         + `&${PAGE_SIZE}=${this.pageSize}`
-  //         + `&${INCLUDE_ONLY_ACTIVE}=${this.includeAll}`;
-  //     if (this.search) {
-  //         url += `&${SEARCH}=${this.search}`;
-  //     }
-  //     if (this.from) {
-  //         url += `&${FROM}=${this.from}`;
-  //     }
-  //     if (this.to) {
-  //         url += `&${TO}=${this.to}`;
-  //     }
-  //     if (this.typeIds && this.typeIds.length > 0) {
-  //         url += `&${TYPE_IDS}=${ this.typeIds.join('&' + TYPE_IDS + '=')}`;
-  //     }
-  //     return url;
-  // }
 }
