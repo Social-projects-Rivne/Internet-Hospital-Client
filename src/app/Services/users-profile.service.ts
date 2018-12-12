ddec66c52cb8fd3948fff27863842dd0467acdfa
serhii_dotsyk <dosyk11@gmail.com>
2018-12-12 20:33:30 +0200

    fixed style to illnessHistory

--- src/app/Services/users-profile.service.ts
+++ src/app/Services/users-profile.service.ts
@@ -4,6 +4,7 @@ import { IllnessHistory } from '../Models/IllnessHistory';
 import { PaginationService } from './pagination.service';
 import { HOST_URL, PATIENT_GET_AVATAR, PATIENT_UPDATE_AVATAR, API_PATIENT, PATIENT_GET_HISTORIES } from '../config';
 import { DatePipe } from '@angular/common';
+import { setMilliseconds } from 'date-fns';
 
 const start = 'searchfromdate';
 const end = 'searchtodate';
@@ -62,10 +63,33 @@ export class UsersProfileService {
                 this.illnessHistories = result.histories;
                 this.illnessHistoriesAmount = result.totalHistories;
                 for (const history of this.illnessHistories) {
-                    history.finishAppointmentTime = new Date(history.finishAppointmentTimeStamp.toString());
-                    console.log(history.finishAppointmentTimeStamp);
+                    history.finishAppointmentTime = new Date(null);
                     console.log(history.finishAppointmentTime);
+                    history.finishAppointmentTime.setTime(history.finishAppointmentTimeStamp.valueOf());
+                    console.log(history.finishAppointmentTimeStamp.valueOf());
+                    console.log(history.finishAppointmentTime);
+
+                    const ticks = history.finishAppointmentTimeStamp.valueOf();
+                    const fff = ticks * 1000;
+                    const todayTicks = new Date().setMilliseconds(fff);
+                    // const diff = fff - todayTicks;
+                    console.log(fff);
+                    console.log(todayTicks);
+                    // console.log(diff);
+                    // const days = Math.floor(diff / (24 * 60 * 60 * 1000));
+                    // const hours = (diff / (60 * 60 * 1000)) % 24;
+                    // const mins = (diff / (60 * 1000)) % 60;
+
+                    // console.log(hours);
+                    const hh = Math.floor(ticks / 3600);
+                    const mm = Math.floor((ticks % 3600) / 60);
+                    const ss = ticks % 60;
+                    // console.log(this.pad(hh, 2) + ':' + this.pad(mm, 2) + ':' + this.pad(ss, 2) );
                 }
             });
+    }
+    pad(n, width) {
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
     }
 }
