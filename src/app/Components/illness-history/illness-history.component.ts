6158f3a359d9ed765cb08d2fb4553a23f149d72e
Andriy Slesarchuk <slesarchukandr@gmail.com>
2018-12-19 14:53:24 +0200

    Add logic to previous url returning

--- src/app/Components/illness-history/illness-history.component.ts
+++ src/app/Components/illness-history/illness-history.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { DoctorsService } from 'src/app/Services/doctors.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, RoutesRecognized, NavigationStart } from '@angular/router';
 import { NotificationService } from 'src/app/Services/notification.service';
-import { MY_PLANS } from 'src/app/config';
+import { PreviousRouteService } from 'src/app/Services/previous-route.service';
+// import 'rxjs/add/operator/pairwise';
+// import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-illness-history',
@@ -14,8 +17,12 @@ export class IllnessHistoryComponent implements OnInit {
 
   private appointmentId: number;
 
-  constructor(private service: DoctorsService, private router: Router, private notification: NotificationService,
-    private activateRoute: ActivatedRoute) { }
+  constructor(private service: DoctorsService,
+    private router: Router,
+    private previousRouteService: PreviousRouteService,
+    private notification: NotificationService,
+    private activateRoute: ActivatedRoute,
+    private location: Location) { }
 
   load = false;
 
@@ -44,26 +51,44 @@ export class IllnessHistoryComponent implements OnInit {
   }
 
   ngOnInit() {
+    // this.router.events
+    // .filter(e => e instanceof RoutesRecognized)
+    // .pairwise()
+    // .subscribe((e) => {
+    //   console.log(e);
+    // });
+
+    // this.router.events.filter(e => e instanceof NavigationStart )
+    //   .bufferCount(1, 1).subscribe(e => console.log(e[0]['url']));
+
+
     this.appointmentId = this.activateRoute.snapshot.params['id'];
     this.initializeFormGroup();
   }
 
   onCancel() {
-    this.router.navigate([MY_PLANS]);
+    console.log(this.previousRouteService.getPreviousUrl());
+    console.log('dsadasdasdasdasd');
+    // this.location.back();
+    // this.router.navigate([MY_PLANS]);
   }
 
   onSubmit() {
+    console.log(this.previousRouteService.getPreviousUrl());
+    console.log('111111111111111111111111111111111111111');
     this.load = true;
     this.service.fillIllness(this.form, this.appointmentId).subscribe(
       () => {
         this.notification.success('All is fine!');
         this.load = false;
-        this.router.navigate([MY_PLANS]);
+        this.location.back();
+        // this.router.navigate([MY_PLANS]);
       },
       (error) => {
         this.notification.error(error);
         this.load = false;
-        this.router.navigate([MY_PLANS]);
+        this.location.back();
+        // this.router.navigate([MY_PLANS]);
       });
   }
 }
