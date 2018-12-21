aea06303462aa0153ca231d0e6b22b6c02d09304
serhii_dotsyk <dosyk11@gmail.com>
2018-12-21 15:06:22 +0200

    edit user profile

---
+++ src/app/Models/UsersEditProfileList.ts
@@ -0,0 +1,6 @@
+import { UsersEditProfileModel } from './UsersEditProfileModel';
+
+export class UsersEditProfileList {
+    entities: UsersEditProfileModel[];
+    entityAmount: number;
+}
---
+++ src/app/Models/UsersEditProfileModel.ts
@@ -0,0 +1,15 @@
+export class UsersEditProfileModel {
+    userId: number;
+    firstName: string;
+    secondName: string;
+    thirdName: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    specialization: string;
+    birthDate: Date;
+    requestTime: Date;
+    passport: string[];
+    diplomas: string[];
+    license: string;
+}
